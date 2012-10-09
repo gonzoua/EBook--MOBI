4bc8dac06476fd744d3ed16e2016c1398d3ac428
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2012-10-08 19:13:16 -0700

    Remove meta tags, they're not required

--- lib/EBook/MOBI.pm
+++ lib/EBook/MOBI.pm
@@ -193,7 +193,6 @@ sub make {
         my $tmp = $self->{html_data};
     $self->{html_data} = "<html>
 <head>
-<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
 </head>
 <body>
 " . $tmp . "</body>\n</html>\n";
@@ -255,7 +254,6 @@ sub _generate_toc {
     my $tmp = $self->{html_data};
     $self->{html_data} = "<html>
 <head>
-<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
 <guide>
 <reference type=\"toc\" title=\"$self->{toc_label}\" filepos=\"00000000\"/>
 </guide>
